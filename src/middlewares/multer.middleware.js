f3576225c27aa3c6cdd6d504d327e418ebd27bf3
Tushar <tushar@Tushars-MacBook-Air.local>
2024-12-10 17:20:35 +0530

    changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,10 +4,10 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp")
     },
-    filename: function (req, file, cb) {
+    // filename: function (req, file, cb) {
       
-      cb(null, file.originalname)
-    }
+    //   cb(null, file.originalname)
+    // }
   })
   
 export const upload = multer({ 
